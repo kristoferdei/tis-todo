df60e96822662dacc7a191ea3f452b28ca023339
kristoferdei <erdeikristof@t-online.hu>
2020-09-24 09:17:44 +0200

    TODO app - react typescript, redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FormEvent, FunctionComponent, useState} from 'react'
+import "./tailwind.output.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Todo {
+    text: string;
 }
 
-export default App;
+const App:FunctionComponent = () => {
+    const [value, setValue] = useState<string>('');
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        addTodo(value);
+        setValue('')
+    };
+
+    const addTodo = (text: string): void => {
+        const newTodos: Todo[] = [...todos, { text }];
+        setTodos(newTodos)
+    };
+
+    const deleteTodo = (index: number): void => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="bg-gray-200 p-5">
+        <>
+            <h1 className="text-3xl font-bold mb-5">Todo</h1>
+            <form onSubmit={handleSubmit}>
+                <input className="text-gray-700 text-sm font-bold mb-2 p-1"
+                       placeholder="Add new todo"
+                       type="text"
+                       value={value}
+                       onChange={e => setValue(e.target.value)}
+                       required
+                />
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mt-5 ml-5"
+                        type="submit">
+                        Add
+                </button>
+            </form>
+            <section>
+                {todos.map((todo: Todo, index: number) => (
+                    <div key={index}>
+                        <input type="checkbox"
+                               className="mr-5"/>
+                               {todo.text}
+                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded mt-5 ml-5"
+                                onClick={() => deleteTodo(index)}>
+                                Delete
+                        </button>
+                    </div>
+                ))}
+            </section>
+        </>
+        </div>
+    );
+};
+
+export default App
---
+++ src/Redux.tsx
@@ -0,0 +1,132 @@
+import React, {FunctionComponent, useState, FormEvent, ChangeEvent} from 'react';
+import {combineReducers, createStore, Store} from 'redux';
+import {Provider, useDispatch, useSelector} from 'react-redux';
+import "./tailwind.output.css"
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type  Todo = {
+  index: number;
+  text: string;
+};
+
+type TodoState = {
+  todos: Todo[];
+};
+
+function addTodo(todoText: string) {
+    return {
+        type: 'AddTodo',
+        payload: todoText
+    } as const;
+}
+
+function deleteTodo(index: number) {
+    return {
+        type: 'DeleteTodo',
+        payload: index
+    } as const;
+}
+
+type Actions = ReturnType<typeof addTodo> | ReturnType<typeof deleteTodo>;
+
+function todoReducer(state: Todo[] = [], action: Actions) {
+    switch (action.type) {
+        case 'AddTodo':
+            return state.concat({ index: state.length + 1, text: action.payload });
+            case 'DeleteTodo':
+                return state.filter(todo => todo.index !== action.payload);
+    }
+    return state;
+}
+
+const rootReducer = combineReducers<TodoState>({
+    todos: todoReducer
+});
+
+function configureStore(): Store<TodoState> {
+    return createStore(rootReducer, undefined);
+}
+
+const store = configureStore();
+
+const Redux:FunctionComponent = () => (
+    <Provider store={store}>
+        <ToDo />
+    </Provider>
+);
+
+const ToDo:FunctionComponent = () => {
+    const [newTodo, setNewTodo] = useState("");
+    const updateNewTodo = () => (e: ChangeEvent<HTMLInputElement>) =>
+        setNewTodo(e.currentTarget.value);
+
+    const todos: Todo[] = useSelector((state: TodoState) => state.todos);
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(addTodo(newTodo));
+        setNewTodo("");
+    };
+
+    const dispatchNewTodo = (index: number) => () => {
+        dispatch(deleteTodo(index));
+    };
+
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <div className="h-screen bg-gradient-to-r from-pink-200 to-purple-400 p-20 flex content-start flex-wrap place-content-center">
+        <div className="h-full w-1/3 p-4 bg-white rounded-lg">
+            <div className="overflow-y-auto h-full transform">
+                <table className="table-fixed w-full">
+                    <thead>
+                    {todos.map(todos => (
+                        <tr key={todos.index}>
+                            <th className="w-1/4 px-4 py-2">
+                                <input type="checkbox" className="mt-2 py-1 px-4"/>
+                            </th>
+                            <th className="font-sans w-1/2 px-4 py-2 text-gray-600 text-black break-words">
+                                {todos.text}
+                            </th>
+                            <th className="w-1/4 px-4 py-2">
+                                <button className="bg-red-500 hover:bg-red-700 text-white rounded-full h-6 w-6 flex items-center justify-center"
+                                        onClick={dispatchNewTodo(todos.index)}>
+                                    <FontAwesomeIcon icon={faTimes}/>
+                                </button>
+                            </th>
+                        </tr>
+                    ))}
+                    </thead>
+                </table>
+            <div className="flex justify-center absolute inset-x-0 bottom-0">
+            <button onClick={() => setVisible(!visible)}
+                    className="bg-yellow-500 hover:bg-yellow-600 text-white text-3xl
+                               rounded-full h-16 w-16">
+                <FontAwesomeIcon icon={faPlus}/>
+            </button>
+            </div>
+            {visible && <form id="form" onSubmit={handleSubmit}>
+                <div className="flex justify-center">
+                    <div className="absolute bottom-0 flex justify-center mb-16">
+                        <input
+                            className="font-sans text-gray-700 text-base font-bold mb-2 p-1
+                                        flex justify-center text-center border-solid border-2
+                                        border-black border-opacity-25 rounded-lg bg-gray-100 tracking-wider hover:bg-white"
+                            placeholder="Add new todo"
+                            value={newTodo}
+                            onChange={updateNewTodo()}
+                            required
+                        />
+                    </div>
+                </div>
+            </form>
+            }
+        </div>
+        </div>
+    </div>
+    );
+};
+
+export default Redux
